eec21cbadef5260d5a58b3d359b49f39954bc7a3
NorthendenAllotments <toadnfrog101@gmail.com>
2025-03-26 11:22:49 +0000

    First Real Commit
    Everything else has been to test and get more familiar with github, so please ignore.

---
+++ quiz.js
@@ -0,0 +1,99 @@
+
+
+//define scoring function
+function checkAnswers(){
+    // set the score, at 0
+    let score = 0;
+    let result = 0;
+
+    //assign the quiz elements to the quiz variable
+    let quiz = document.forms.quiz.elements;
+    
+    //assign the score for question 1 to the answer1 variable
+    let answer1 = quiz.colour.value;
+
+    //assign the score value
+    if(answer1 === "pink"){
+        score = score + 1;
+    }
+    else if (answer1 === "yellow") {
+        score = score + 2;
+    }
+    else if (answer1 === "green") {
+        score = score + 3;
+    }
+    else {
+        score = score + 4;
+    }
+
+    //assign the score for question 2 to the answer2 variable
+    let answer2 = quiz.animal.value;
+
+    //assign the score value
+    if(answer2 === "red-panda"){
+        score = score + 1;
+    }
+    else if (answer2 === "giraffe") {
+        score = score + 2;
+    }
+    else if (answer2 === "jaguar") {
+        score = score + 3;
+    }
+    else {
+        score = score + 4;
+    }
+
+    //assign the score for question 3 to the answer3 variable
+    let answer3 = quiz.music.value;
+
+    //assign the score value
+    if(answer3 === "pop"){
+        score = score + 1;
+    }
+    else if (answer3 === "hip-hop") {
+        score = score + 2;
+    }
+    else if (answer3 === "rock") {
+        score = score + 3;
+    }
+    else {
+        score = score + 4;
+    }
+
+    //assign the score for question 4 to the answer4 variable
+    let answer4 = quiz.hobby.value;
+
+    //assign the score value
+    if(answer4 === "read"){
+        score = score + 1;
+    }
+    else if (answer4 === "music") {
+        score = score + 2;
+    }
+    else if (answer4 === "walk") {
+        score = score + 3;
+    }
+    else {
+        score = score + 4;
+    }
+
+    //determine result from score
+    if (score < 6){
+        result = "HTML"
+    }
+
+    else if (score < 10){
+        result = "Python"
+    }
+
+    else if (score <= 13){
+        result = "JavaScript"
+    }
+
+    else {
+        result = "Java"
+    }
+    
+    //display result as an alert.
+    alert(`The Programming Language you should learn in ${result}!`);
+}
